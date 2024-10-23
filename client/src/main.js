97113bd4ee7abbb347fb86f1699dcfae2bd8fe79
Слава Матвеев <slava.matveev@gmail.com>
2024-10-23 23:51:01 +0300

    Structure

--- client/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router' // Убедись, что это правильный путь
-
-createApp(App)
-  .use(router)
-  .mount('#app')
--- client2/twitter/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div id="app">
-    <nav>
-      <router-link to="/create">Создать твит</router-link>
-      <router-link to="/view">Просмотреть твиты</router-link>
-    </nav>
-    <router-view />
-  </div>
-</template>
